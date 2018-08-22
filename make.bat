b9ae2f81ce879b97b5f874b82ceb0dee6e486aac
Crawl <icrawltogo@gmail.com>
2018-08-22 16:51:45 +0200

    chore: add support for older/more node versions

--- make.bat
+++ make.bat
@@ -1,15 +1,17 @@
-call "%VS140COMNTOOLS%..\..\vc\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
 if not exist targets (
 mkdir targets
-curl https://nodejs.org/dist/v6.4.0/node-v6.4.0-headers.tar.gz | tar xz -C targets
-curl https://nodejs.org/dist/v6.4.0/win-x64/node.lib > targets/node-v6.4.0/node.lib
-curl https://nodejs.org/dist/v7.1.0/node-v7.1.0-headers.tar.gz | tar xz -C targets
-curl https://nodejs.org/dist/v7.1.0/win-x64/node.lib > targets/node-v7.1.0/node.lib
-curl https://nodejs.org/dist/v8.1.2/node-v8.1.2-headers.tar.gz | tar xz -C targets
-curl https://nodejs.org/dist/v8.1.2/win-x64/node.lib > targets/node-v8.1.2/node.lib
-curl https://nodejs.org/dist/v9.2.0/node-v9.2.0-headers.tar.gz | tar xz -C targets
-curl https://nodejs.org/dist/v9.2.0/win-x64/node.lib > targets/node-v9.2.0/node.lib
+curl https://nodejs.org/dist/v6.14.4/node-v6.14.4-headers.tar.gz | tar xz -C targets
+curl https://nodejs.org/dist/v6.14.4/win-x64/node.lib > targets/node-v6.14.4/node.lib
+curl https://nodejs.org/dist/v7.10.1/node-v7.10.1-headers.tar.gz | tar xz -C targets
+curl https://nodejs.org/dist/v7.10.1/win-x64/node.lib > targets/node-v7.10.1/node.lib
+curl https://nodejs.org/dist/v8.11.4/node-v8.11.4-headers.tar.gz | tar xz -C targets
+curl https://nodejs.org/dist/v8.11.4/win-x64/node.lib > targets/node-v8.11.4/node.lib
+curl https://nodejs.org/dist/v9.11.2/node-v9.11.2-headers.tar.gz | tar xz -C targets
+curl https://nodejs.org/dist/v9.11.2/win-x64/node.lib > targets/node-v9.11.2/node.lib
+curl https://nodejs.org/dist/v10.9.0/node-v10.9.0-headers.tar.gz | tar xz -C targets
+curl https://nodejs.org/dist/v10.9.0/win-x64/node.lib > targets/node-v10.9.0/node.lib
 )
 
 cp README.md dist/README.md
@@ -20,11 +22,14 @@ cp src/addon.h dist/src/addon.h
 cp src/http.h dist/src/http.h
 cp src/uws.js dist/uws.js
 
-cl /I targets/node-v6.4.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_48.node dist/src/*.cpp targets/node-v6.4.0/node.lib
-cl /I targets/node-v7.1.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_51.node dist/src/*.cpp targets/node-v7.1.0/node.lib
-cl /I targets/node-v8.1.2/include/node /EHsc /Ox /LD /Fedist/uws_win32_57.node dist/src/*.cpp targets/node-v8.1.2/node.lib
-cl /I targets/node-v9.2.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_59.node dist/src/*.cpp targets/node-v9.2.0/node.lib
+cl /I targets/node-v6.14.4/include/node /EHsc /Ox /LD /Fedist/uws_win32_48.node dist/src/*.cpp targets/node-v6.14.4/node.lib
+cl /I targets/node-v7.10.1/include/node /EHsc /Ox /LD /Fedist/uws_win32_51.node dist/src/*.cpp targets/node-v7.10.1/node.lib
+cl /I targets/node-v8.11.4/include/node /EHsc /Ox /LD /Fedist/uws_win32_57.node dist/src/*.cpp targets/node-v8.11.4/node.lib
+cl /I targets/node-v9.11.2/include/node /EHsc /Ox /LD /Fedist/uws_win32_59.node dist/src/*.cpp targets/node-v9.11.2/node.lib
+cl /I targets/node-v10.9.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_64.node dist/src/*.cpp targets/node-v10.9.0/node.lib
 
 rm *.obj
 rm dist/*.exp
 rm dist/*.lib
+rm -rf dist/src
+rm -rf targets
