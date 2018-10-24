8039ed2218f5b6d79384d37a5db53f1a05ec70cb
Crawl <icrawltogo@gmail.com>
2018-10-24 02:00:11 +0200

    feat: node 11 builds

--- Makefile
+++ Makefile
@@ -5,9 +5,10 @@ default:
 	make targets
 	NODE=targets/node-v6.14.4 ABI=48 make `(uname -s)`
 	NODE=targets/node-v7.10.1 ABI=51 make `(uname -s)`
-	NODE=targets/node-v8.11.4 ABI=57 make `(uname -s)`
+	NODE=targets/node-v8.12.0 ABI=57 make `(uname -s)`
 	NODE=targets/node-v9.11.2 ABI=59 make `(uname -s)`
-	NODE=targets/node-v10.9.0 ABI=64 make `(uname -s)`
+	NODE=targets/node-v10.12.0 ABI=64 make `(uname -s)`
+	NODE=targets/node-v11.0.0 ABI=67 make `(uname -s)`
 	cp README.md dist/README.md
 	cp ./uWebSockets/LICENSE dist/LICENSE
 	cp -r ./uWebSockets/src dist/
@@ -20,9 +21,10 @@ targets:
 	mkdir targets
 	curl https://nodejs.org/dist/v6.14.4/node-v6.14.4-headers.tar.gz | tar xz -C targets
 	curl https://nodejs.org/dist/v7.10.1/node-v7.10.1-headers.tar.gz | tar xz -C targets
-	curl https://nodejs.org/dist/v8.11.4/node-v8.11.4-headers.tar.gz | tar xz -C targets
+	curl https://nodejs.org/dist/v8.12.0/node-v8.12.0-headers.tar.gz | tar xz -C targets
 	curl https://nodejs.org/dist/v9.11.2/node-v9.11.2-headers.tar.gz | tar xz -C targets
-	curl https://nodejs.org/dist/v10.9.0/node-v10.9.0-headers.tar.gz | tar xz -C targets
+	curl https://nodejs.org/dist/v10.12.0/node-v10.12.0-headers.tar.gz | tar xz -C targets
+	curl https://nodejs.org/dist/v11.0.0/node-v11.0.0-headers.tar.gz | tar xz -C targets
 Linux:
 	g++ $(CPP_SHARED) -static-libstdc++ -static-libgcc -I $$NODE/include/node -s -o dist/uws_linux_$$ABI.node
 Darwin:
--- make.bat
+++ make.bat
@@ -6,12 +6,14 @@ curl https://nodejs.org/dist/v6.14.4/node-v6.14.4-headers.tar.gz | tar xz -C tar
 curl https://nodejs.org/dist/v6.14.4/win-x64/node.lib > targets/node-v6.14.4/node.lib
 curl https://nodejs.org/dist/v7.10.1/node-v7.10.1-headers.tar.gz | tar xz -C targets
 curl https://nodejs.org/dist/v7.10.1/win-x64/node.lib > targets/node-v7.10.1/node.lib
-curl https://nodejs.org/dist/v8.11.4/node-v8.11.4-headers.tar.gz | tar xz -C targets
-curl https://nodejs.org/dist/v8.11.4/win-x64/node.lib > targets/node-v8.11.4/node.lib
+curl https://nodejs.org/dist/v8.12.0/node-v8.12.0-headers.tar.gz | tar xz -C targets
+curl https://nodejs.org/dist/v8.12.0/win-x64/node.lib > targets/node-v8.12.0/node.lib
 curl https://nodejs.org/dist/v9.11.2/node-v9.11.2-headers.tar.gz | tar xz -C targets
 curl https://nodejs.org/dist/v9.11.2/win-x64/node.lib > targets/node-v9.11.2/node.lib
-curl https://nodejs.org/dist/v10.9.0/node-v10.9.0-headers.tar.gz | tar xz -C targets
-curl https://nodejs.org/dist/v10.9.0/win-x64/node.lib > targets/node-v10.9.0/node.lib
+curl https://nodejs.org/dist/v10.12.0/node-v10.12.0-headers.tar.gz | tar xz -C targets
+curl https://nodejs.org/dist/v10.12.0/win-x64/node.lib > targets/node-v10.12.0/node.lib
+curl https://nodejs.org/dist/v11.0.0/node-v11.0.0-headers.tar.gz | tar xz -C targets
+curl https://nodejs.org/dist/v11.0.0/win-x64/node.lib > targets/node-v11.0.0/node.lib
 )
 
 cp README.md dist/README.md
@@ -24,9 +26,10 @@ cp src/uws.js dist/uws.js
 
 cl /I targets/node-v6.14.4/include/node /EHsc /Ox /LD /Fedist/uws_win32_48.node dist/src/*.cpp targets/node-v6.14.4/node.lib
 cl /I targets/node-v7.10.1/include/node /EHsc /Ox /LD /Fedist/uws_win32_51.node dist/src/*.cpp targets/node-v7.10.1/node.lib
-cl /I targets/node-v8.11.4/include/node /EHsc /Ox /LD /Fedist/uws_win32_57.node dist/src/*.cpp targets/node-v8.11.4/node.lib
+cl /I targets/node-v8.12.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_57.node dist/src/*.cpp targets/node-v8.12.0/node.lib
 cl /I targets/node-v9.11.2/include/node /EHsc /Ox /LD /Fedist/uws_win32_59.node dist/src/*.cpp targets/node-v9.11.2/node.lib
-cl /I targets/node-v10.9.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_64.node dist/src/*.cpp targets/node-v10.9.0/node.lib
+cl /I targets/node-v10.12.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_64.node dist/src/*.cpp targets/node-v10.12.0/node.lib
+cl /I targets/node-v11.0.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_67.node dist/src/*.cpp targets/node-v11.0.0/node.lib
 
 rm *.obj
 rm dist/*.exp
